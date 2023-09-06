97afccf4b3930619ac2c9e58ba1a291961cf6eaf
Shin Sungyun <sinsungyun1630@gmail.com>
2023-09-06 15:18:33 +0900

    hihi

--- src/pages/GoodsList.jsx
+++ src/pages/GoodsList.jsx
@@ -26,11 +26,11 @@ const GoodsList = () => {
     const bookingUrl = `${process.env.REACT_APP_URL}/api/booking/${goodsid.id}`;
     // console.log(bookingUrl);
     const access_token = cookies.get('Authorization');
+    console.log(access_token);
     if (!access_token) {
       alert('로그인이 필요합니다.');
       return;
     }
-    console.log(access_token);
     axios
       .post(
         bookingUrl,
